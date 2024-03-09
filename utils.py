4abf826d1d375cb7ea4447ebb1a3b458c09414b6
Eliss Hui <eliss.hui@mail.utoronto.ca>
2024-03-08 21:17:16 -0500

    Added lookahead_pytorch from paper; Updated utils

--- utils.py
+++ utils.py
@@ -1,12 +1,15 @@
 # standard library imports
 import os
 import sys
+from collections import defaultdict
 
 # third-party library imports
 import numpy as np
 import time
 import torch
 import torch.nn as nn
+from torch.optim.optimizer import Optimizer
 
 # classes
 from run_model import runModel
+from lookahead_pytorch import Lookahead
