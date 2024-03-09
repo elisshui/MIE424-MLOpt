b9092c7fffb8ad28a9f048ccfe299434972d1161
Eliss Hui <80904191+elisshui@users.noreply.github.com>
2024-03-09 00:17:51 -0500

    Updated example usage in main.py

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ def main():
     Creates instance of model class, trains with different optimizers (Adam, Lookahead(Adam)), then displays results. 
         - Example usage:
             model = Model()
-            opt_lookahead = Lookahead(adam)
-            run_model_lookahead = runModel(model, opt_lookahead)
+            optimizer = optim.Adam(model.parameters())
+            lookaheadArgs_1 = lookaheadArgs(lookahead=True)
+            run_model_lookahead = runModel(model, optimizer, lookaheadArgs)
             run_model_lookahead.train()
             run_model_lookahead.plot_loss()
     """
