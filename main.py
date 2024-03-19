3d9830df850709e5b0206d0f3f2fa362f2e33b81
EdwardY <76181358+EdwardY@users.noreply.github.com>
2024-03-19 17:46:45 -0400

    bug fix

--- main.py
+++ main.py
@@ -38,8 +38,8 @@ def main():
     
     #running the lookahead model
     model2 = LSTM()
-    lookaheadArgs_1 = lookaheadArgs(lookahead=True)
-    run_model2 = runModel(model2, optimizer, lookaheadArgs_1)
+    lookaheadArgs_2 = lookaheadArgs(lookahead=True)
+    run_model2 = runModel(model2, optimizer, lookaheadArgs_2)
     run_model2.train(train_loader, val_loader)
     run_model2.plot_loss() #generates graphs of both training and validation loss
 
